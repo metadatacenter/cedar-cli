5ddd020a562178da6cce4b8543b0c5df30cd9b64
Attila L. Egyedi <egyedia@stanford.edu>
2023-06-28 18:37:29 -0700

    Properly create branch

--- org/metadatacenter/operator/ReleasePrepareCreateBranchOperator.py
+++ org/metadatacenter/operator/ReleasePrepareCreateBranchOperator.py
@@ -1,9 +1,11 @@
 from rich.console import Console
 
 from org.metadatacenter.model.PlanTask import PlanTask
+from org.metadatacenter.model.PrePostType import PrePostType
 from org.metadatacenter.model.PreReleaseBranchType import PreReleaseBranchType
 from org.metadatacenter.model.TaskType import TaskType
 from org.metadatacenter.operator.Operator import Operator
+from org.metadatacenter.taskfactory.BuildShellTaskFactory import BuildShellTaskFactory
 from org.metadatacenter.taskfactory.ReleasePrepareCreateBranchShellTaskFactory import ReleasePrepareCreateBranchShellTaskFactory
 
 console = Console()
@@ -23,6 +25,11 @@ class ReleasePrepareCreateBranchOperator(Operator):
         repo_list = [task.repo]
 
         for repo in repo_list:
-            shell_wrapper = PlanTask("Create branch for project", TaskType.SHELL_WRAPPER, repo)
-            shell_wrapper.add_task_as_task(ReleasePrepareCreateBranchShellTaskFactory.create_branch(repo, branch_type))
-            task.add_task_as_task(shell_wrapper)
+            if repo.pre_post_type == PrePostType.SUB:
+                not_handled = PlanTask("Skip sub repo", TaskType.NOOP, repo)
+                not_handled.add_task_as_task(BuildShellTaskFactory.noop(repo))
+                task.add_task_as_task(not_handled)
+            elif repo.pre_post_type == PrePostType.NONE or repo.pre_post_type == PrePostType.PRE:
+                shell_wrapper = PlanTask("Create branch for project", TaskType.SHELL_WRAPPER, repo)
+                shell_wrapper.add_task_as_task(ReleasePrepareCreateBranchShellTaskFactory.create_branch(repo, branch_type))
+                task.add_task_as_task(shell_wrapper)
