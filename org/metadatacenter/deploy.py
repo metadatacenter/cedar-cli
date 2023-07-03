52db5e84849bf23f841b3e128b4db50079b7e457
Attila L. Egyedi <egyedia@stanford.edu>
2023-07-03 14:16:15 -0700

    Fix parameters

--- org/metadatacenter/deploy.py
+++ org/metadatacenter/deploy.py
@@ -13,68 +13,76 @@ plan_executor = PlanExecutor()
 
 @app.command("this")
 def this(wd: str = typer.Option(None, help="Working directory"),
-         dry_run: bool = typer.Option(False, help="Dry run")
+         dry_run: bool = typer.Option(False, help="Dry run"),
+         dump_plan: bool = typer.Option(False, help="Dump plan")
          ):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy this")
     DeployPlanner.this(plan, wd)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("parent")
-def parent(dry_run: bool = typer.Option(False, help="Dry run")):
+def parent(dry_run: bool = typer.Option(False, help="Dry run"),
+           dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy parent")
     DeployPlanner.parent(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("libraries")
-def libraries(dry_run: bool = typer.Option(False, help="Dry run")):
+def libraries(dry_run: bool = typer.Option(False, help="Dry run"),
+              dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy libraries")
     DeployPlanner.libraries(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("project")
-def project(dry_run: bool = typer.Option(False, help="Dry run")):
+def project(dry_run: bool = typer.Option(False, help="Dry run"),
+            dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy project")
     DeployPlanner.project(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("clients")
-def clients(dry_run: bool = typer.Option(False, help="Dry run")):
+def clients(dry_run: bool = typer.Option(False, help="Dry run"),
+            dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy clients")
     DeployPlanner.clients(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("java")
-def java(dry_run: bool = typer.Option(False, help="Dry run")):
+def java(dry_run: bool = typer.Option(False, help="Dry run"),
+         dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy java")
     DeployPlanner.parent(plan)
     DeployPlanner.libraries(plan)
     DeployPlanner.project(plan)
     DeployPlanner.clients(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("frontends")
-def frontends(dry_run: bool = typer.Option(False, help="Dry run")):
+def frontends(dry_run: bool = typer.Option(False, help="Dry run"),
+              dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy frontends")
     DeployPlanner.frontends(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
 
 
 @app.command("all")
-def deploy_all(dry_run: bool = typer.Option(False, help="Dry run")):
+def deploy_all(dry_run: bool = typer.Option(False, help="Dry run"),
+               dump_plan: bool = typer.Option(False, help="Dump plan")):
     GlobalContext.mark_global_task_type(TaskType.DEPLOY)
     plan = Plan("Deploy all")
     DeployPlanner.all(plan)
-    plan_executor.execute(plan, dry_run)
+    plan_executor.execute(plan, dry_run, dump_plan)
