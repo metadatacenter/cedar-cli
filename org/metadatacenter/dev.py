1591a8ad17c445a2ee8f57a518f014ae753db18c
Attila L. Egyedi <egyedia@stanford.edu>
2023-07-27 12:12:06 +0200

    Add hosts

--- org/metadatacenter/dev.py
+++ org/metadatacenter/dev.py
@@ -8,3 +8,8 @@ app = typer.Typer(no_args_is_help=True)
 @app.command("create-directories")
 def create_directories():
     DevWorker.create_directories()
+
+
+@app.command("add-hosts")
+def add_hosts():
+    DevWorker.add_hosts()
