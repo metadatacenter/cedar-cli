f046b0b9d7a2334deb6c53835976eb59c5c3b1c1
Attila L. Egyedi <egyedia@stanford.edu>
2023-08-25 13:36:15 -0700

    Documentation

--- cedar.py
+++ cedar.py
@@ -8,29 +8,29 @@ from org.metadatacenter.worker.ServerWorker import ServerWorker
 GlobalContext()
 
 app = typer.Typer(no_args_is_help=True)
-app.add_typer(repo.app, name="repo", help="Git repos vs working directories")
-app.add_typer(git.app, name="git")
-app.add_typer(server.app, name="server")
-app.add_typer(build.app, name="build")
-app.add_typer(deploy.app, name="deploy")
-app.add_typer(clean.app, name="clean")
-app.add_typer(env.app, name="env")
-app.add_typer(release.app, name="release")
-app.add_typer(start.app, name="start")
-app.add_typer(stop.app, name="stop")
-app.add_typer(check.app, name="check")
-app.add_typer(docker.app, name="docker")
-app.add_typer(dev.app, name="dev")
-app.add_typer(prod.app, name="prod")
-app.add_typer(cert.app, name="cert")
+app.add_typer(repo.app, name="repo", help="Configured repo info...")
+app.add_typer(git.app, name="git", help="Git operations on all repos...")
+app.add_typer(server.app, name="server", help="Server status...")
+app.add_typer(build.app, name="build", help="Build various components...")
+app.add_typer(deploy.app, name="deploy", help="Deploy various components...")
+app.add_typer(clean.app, name="clean", help="Clean various locations...")
+app.add_typer(env.app, name="env", help="List environment variables...")
+app.add_typer(release.app, name="release", help="Create a CEDAR release...")
+app.add_typer(start.app, name="start", help="Start various components...")
+app.add_typer(stop.app, name="stop", help="Stop various components...")
+app.add_typer(check.app, name="check", help="Check various artifacts...")
+app.add_typer(docker.app, name="docker", help="Docker related operations...")
+app.add_typer(dev.app, name="dev", help="Development related operations...")
+app.add_typer(prod.app, name="prod", help="Production server related operations...")
+app.add_typer(cert.app, name="cert", help="Self-signed certificates...")
 
 
-@app.command("cheat")
+@app.command("cheat", help="Open cheatsheet")
 def cheat():
     CheatWorker.cheat()
 
 
-@app.command("status")
+@app.command("status", help="Shortcut for 'server status'")
 def status():
     ServerWorker.status()
 
